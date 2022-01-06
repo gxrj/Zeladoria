162cc9c8ddfa3cb062e6ad01440fb76e25a42267
gxrj <gsouzafone@gmail.com>
2022-01-05 23:02:12 -0300

    Setting security configurations

--- backend/auth-server/src/main/java/com/femass/authserver/auth/domain/abstracts/Conta.java
+++ backend/auth-server/src/main/java/com/femass/authserver/auth/domain/abstracts/Conta.java
@@ -1,13 +1,11 @@
 package com.femass.authserver.auth.domain.abstracts;
 
 import java.util.UUID;
-
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
