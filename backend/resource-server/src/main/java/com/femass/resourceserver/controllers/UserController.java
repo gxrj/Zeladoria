e529a9bda4bfa108292cd3724d7ad51088ae46cb
gxrj <gsouzafone@gmail.com>
2022-02-22 11:06:48 -0300

    fix

--- backend/resource-server/src/main/java/com/femass/resourceserver/controllers/UserController.java
+++ backend/resource-server/src/main/java/com/femass/resourceserver/controllers/UserController.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,4 +57,7 @@ public class UserController {
         else 
             return "{\"message\":\"Error\"}";
     }
+    
+    @GetMapping( "/user/test" )
+    public String test() { return "{\"message\":\"Authenticated\"}"; }
 }
