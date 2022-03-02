954f8a647558bbb368c1e18942b6ebbb7be02213
gxrj <gsouzafone@gmail.com>
2022-03-01 22:08:11 -0300

    *

--- backend/authz-server/src/main/java/com/femass/authzserver/auth/filters/UniqueTokenSessionFilter.java
+++ backend/authz-server/src/main/java/com/femass/authzserver/auth/filters/UniqueTokenSessionFilter.java
@@ -3,11 +3,9 @@ package com.femass.authzserver.auth.filters;
 import com.femass.authzserver.auth.services.InMemoryTokenService;
 
 import com.nimbusds.jose.JWSObject;
-import com.nimbusds.jose.PlainObject;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.OAuth2TokenType;
-import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 
 import org.springframework.util.Assert;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -40,22 +38,32 @@ public class UniqueTokenSessionFilter extends OncePerRequestFilter  {
         var token = request.getHeader( "authorization" );
         var code = request.getParameter( "code" );
 
+        //avoid multiple session creation
         if( code != null ) {
-            //Todo: avoid multiple sessions
+
+            var currentSession = authorizationService
+                                    .findByToken( code, new OAuth2TokenType( "code" ) );
+
+            if( currentSession != null ) {
+                var currentToken = currentSession.getAccessToken().getToken().getTokenValue();
+                var principal = ( Authentication ) currentSession.getAttribute( Principal.class.getName() );
+                Assert.notNull( principal, "principal cannot be null" );
+                var list = authorizationService.findByPrincipalName( principal.getName() );
+            }
         }
-        if( token != null  ) {
-            authzServiceInMemory( request, response, authorizationService, token );
+        // invalidate older tokens right after get a new one
+//        if( token != null  ) {
+//            authzServiceInMemory( authorizationService, token );
 //            if ( authorizationService instanceof InMemoryTokenService ) {
 //                authzServiceInMemory( request, response, authorizationService, token );
 //            }
 //            else if ( authorizationService instanceof JdbcOAuth2AuthorizationService ) {
-//                jdbcAuthzService( request, response, authorizationService, token );
+//                jdbcAuthzService( authorizationService, token );
 //            }
-        }
+//        }
     }
 
-    private void authzServiceInMemory(  HttpServletRequest req, HttpServletResponse res,
-                                        InMemoryTokenService authorizationService, String token ) {
+    private void authzServiceInMemory( InMemoryTokenService authorizationService, String token ) {
 
         try {
 
@@ -72,9 +80,9 @@ public class UniqueTokenSessionFilter extends OncePerRequestFilter  {
             jwtSessions.forEach(
                     jwtSession -> {
                         var access = jwtSession.getAccessToken().getToken().getTokenValue();
-                        var refresh = jwtSession.getRefreshToken().getToken();
+                        var refresh = jwtSession.getRefreshToken();
                         Assert.notNull( refresh, "refresh is null" );
-                        System.out.println( "access: "+access+"\nrefresh: "+ refresh.getTokenValue() );
+                        System.out.println( "access: "+access+"\nrefresh: "+ refresh.getToken().getTokenValue() );
                     }
             );
 
@@ -84,8 +92,7 @@ public class UniqueTokenSessionFilter extends OncePerRequestFilter  {
         }
     }
 
-    private void jdbcAuthzService( HttpServletRequest req, HttpServletResponse res,
-                                   InMemoryTokenService authorizationService, String token ) {
+    private void jdbcAuthzService( InMemoryTokenService authorizationService, String token ) {
         System.out.println( "todo database" );
     }
 
