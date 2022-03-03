df3c442c75c37b79aa312232bd1c7a0b3263c510
gxrj <gsouzafone@gmail.com>
2022-03-03 03:22:42 -0300

    In progress

--- backend/resource-server/src/main/java/com/femass/resourceserver/domain/Address.java
+++ backend/resource-server/src/main/java/com/femass/resourceserver/domain/Address.java
@@ -1,5 +1,30 @@
 package com.femass.resourceserver.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Address {
-    
+
+    @Column( name = "latitude" )
+    private Double latitude;
+    @Column( name = "longitude" )
+    private Double longitude;
+    @Column( name = "cep", length = 8 )
+    private String zipCode;
+    @Column( name = "logradouro" )
+    private String publicPlace;
+    @Column( name = "bairro" )
+    private String district;
+    @Column( name = "referencia", length = 80 )
+    private String reference;
 }
--- backend/resource-server/src/main/java/com/femass/resourceserver/domain/Image.java
+++ backend/resource-server/src/main/java/com/femass/resourceserver/domain/Image.java
@@ -1,4 +1,23 @@
 package com.femass.resourceserver.domain;
 
-public class Image {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Image implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private String identifier;
+    private byte[] content;
 }
