bf6bbf766f9a2a858ce724dbeb0438e9565042f3
gxrj <gsouzafone@gmail.com>
2022-03-03 12:03:39 -0300

    In progress

--- backend/resource-server/src/main/java/com/femass/resourceserver/domain/Address.java
+++ backend/resource-server/src/main/java/com/femass/resourceserver/domain/Address.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
 
 @Getter
 @Setter
@@ -13,6 +14,7 @@ import javax.persistence.Column;
 @AllArgsConstructor
 @NoArgsConstructor
 
+@Embeddable
 public class Address {
 
     @Column( name = "latitude" )
