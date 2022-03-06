1f220cf21218392b8cd5a236735cfca4ce5b3af3
gxrj <gsouzafone@gmail.com>
2022-03-06 02:27:16 -0300

    In progress

--- backend/resource-server/src/main/java/com/femass/resourceserver/controllers/CallController.java
+++ backend/resource-server/src/main/java/com/femass/resourceserver/controllers/CallController.java
@@ -1,7 +1,21 @@
 package com.femass.resourceserver.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(
+        path = "/user/calls",
+        consumes = MediaType.ALL_VALUE,
+        produces = MediaType.ALL_VALUE
+)
 public class CallController {
+
+    //TODO
+    @PostMapping( "/new" )
+    public String create() {
+        return "{\"message\": \"call created!\"}";
+    }
 }
