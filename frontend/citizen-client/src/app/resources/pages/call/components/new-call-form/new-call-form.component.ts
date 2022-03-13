7ce507a1c09915e6874aefa2cd2e4a5602a74e37
gxrj <gsouzafone@gmail.com>
2022-03-13 09:39:07 -0300

    *

--- frontend/citizen-client/src/app/resources/pages/call/components/new-call-form/new-call-form.component.ts
+++ frontend/citizen-client/src/app/resources/pages/call/components/new-call-form/new-call-form.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import Call from '@app/core/interfaces/call';
-import Duty from '@app/core/interfaces/duty';
-import { AuthService } from '@app/shared/services/auth/auth.service';
-import { CallService } from '@app/shared/services/call/call.service';
-import { TokenStorageService } from '@app/shared/services/token-storage/token-storage.service';
-import { UserService } from '@app/shared/services/user/user.service';
+
+import Call from '@core/interfaces/call';
+import Duty from '@core/interfaces/duty';
+
+import { CallService } from '@services/call/call.service';
+import { UserService } from '@services/user/user.service';
 
 @Component({
   selector: 'new-call-form',
@@ -20,8 +20,6 @@ export class NewCallFormComponent implements OnInit {
   call: Call
 
   constructor( private _fb: FormBuilder,
-               private _authService: AuthService,
-               private _tokenStorage: TokenStorageService,
                private _userService: UserService,
                private _callService: CallService ) {
 
@@ -37,7 +35,6 @@ export class NewCallFormComponent implements OnInit {
 
   ngOnInit() {
     this.form.patchValue( { service: this.selectedDuty?.description }  )
-    console.log( this.form.valid )
   }
 
   sendData() {
@@ -49,11 +46,17 @@ export class NewCallFormComponent implements OnInit {
 
     let email = this._userService.getUserEmailFromToken()
     if( email == undefined || !email ) email = "anonimo@fiscaliza.com"
+
     this.call = { ...this.form.value }
-    this.call.address =  { ...this.form.value }
+    this.call.address = { ...this.form.value }
     this.call.author = { username: email }
     this.call.status = "Em andamento"
 
-    console.log( this.call )
+    this._callService.create( this.call )
+        .subscribe( {
+          next: request => console.log(request),
+          error: err => console.log(err)
+        } )
+
   }
 }
--- frontend/citizen-client/src/app/shared/services/call/call.service.ts
+++ frontend/citizen-client/src/app/shared/services/call/call.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import Call from '@app/core/interfaces/call';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CallService {
 
-  constructor() { }
+  constructor( private _http: HttpClient, 
+               private _authService: AuthService ) { }
+
+  create( call: Call ) {
+    const request = this._authService.prepareRequest( '/anonymous/calls' )
+    return this._http.post( request.url, call, request.headers )
+  }
 }
