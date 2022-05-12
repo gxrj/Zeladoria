b386ff6cfa8f10564cb597d88a8a3e9a5b34fd89
gxrj <gsouzafone@gmail.com>
2022-05-11 21:32:01 -0300

    *

--- frontend/agent-client/src/app/resources/pages/home/components/forms/attendance-form-view/attendance-form-view.component.ts
+++ frontend/agent-client/src/app/resources/pages/home/components/forms/attendance-form-view/attendance-form-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-attendance-form-view',
@@ -7,8 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AttendanceFormViewComponent implements OnInit {
 
-  constructor() { }
+  dateFormat = 'dd/MM/y'
+  timeFormat = 'HH:mm'
+
+  constructor( 
+    private _modal: ModalController ) { }
 
   ngOnInit() {}
 
+  close() {
+    this._modal.dismiss()
+  }
 }
