902120f174e78abdb61a91d4806d21e260e7fe23
gxrj <gsouzafone@gmail.com>
2022-05-18 20:41:29 -0300

    inits android app, no tests

---
+++ frontend/citizen-client/android/app/src/main/java/io/ionic/fiscaliza/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.fiscaliza;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/citizen-client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.fiscaliza',
+  appName: 'fiscaliza',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
