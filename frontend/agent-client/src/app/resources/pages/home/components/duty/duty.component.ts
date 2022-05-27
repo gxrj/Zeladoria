a58c28cfa0ad9faa7f6f2f59b610cb01250689cc
gxrj <gsouzafone@gmail.com>
2022-05-26 23:33:36 -0300

    *

--- frontend/agent-client/src/app/resources/pages/home/components/duty/duty.component.ts
+++ frontend/agent-client/src/app/resources/pages/home/components/duty/duty.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 import Duty from '@core/interfaces/duty';
 import User from '@core/interfaces/user';
-import { ModalController } from '@ionic/angular';
+import { DepartmentService } from '@services/department/department.service';
+import { ToastService } from '@services/toast/toast.service';
+import Department from '@core/interfaces/department';
 
 @Component({
   selector: 'duty',
@@ -16,27 +19,62 @@ export class DutyComponent implements OnInit {
   modalOpen: boolean = false
   titles = [ 'Descrição', 'Secretaria', 'Ações' ]
 
+  account: User
+  deptList: any = null
+  newDept: String
+  oldDept: String
+  editDepartment: boolean = false
+
   constructor(
-    private _modal: ModalController ) { }
+    private _toast: ToastService,
+    private _modal: ModalController,
+    private _deptService: DepartmentService ) { }
 
   ngOnInit() {
+    this.account = JSON.parse( sessionStorage.getItem( 'user' ) )
     this.loadDepartmentDuties()
+    if( this.account.department === 'Inova Macae' )
+      this.getDepartments()
   }
 
   private loadDepartmentDuties() {
-    const user: User = JSON.parse( sessionStorage.getItem( 'user' ) )
     let array = JSON.parse( sessionStorage.getItem( 'duties' ) )
-    let toFilter = user.department === 'Inova Macae'
-    this.duties = toFilter ? array : array.filter( el => el.department.name === user.department )
+    let toFilter = this.account.department === 'Inova Macae'
+    this.duties = toFilter ? array : array.filter( el => el.department.name === this.account.department )
   }
 
   selectDuty( duty: Duty ) {
     this.selectedDuty = duty
     this.modalOpen = true
+    this.oldDept = duty.department['name'] 
+    this.newDept= this.deptList[0].name
   }
 
   closeModal() {
     this._modal.dismiss()
     this.modalOpen = false
+  }
+
+  getDepartments() {
+    this.deptList = sessionStorage.getItem( 'deptList' )
+    if( this.deptList == null ) {
+      this._deptService.list()
+                .subscribe(
+                  res => this.deptList = res.result,
+                  err => this._toast.displayMessage( err ) )
+    }
+    else
+      this.deptList = JSON.parse( this.deptList )
+  }
+
+  hide( dept: Department ): boolean{
+    return dept.name === 'Inova Macae' ? true : false
+  }
+
+  save( duty: Duty ) {
+    if( this.editDepartment )
+      duty['department']['name'] = this.newDept
+
+    console.log( duty )
   }
 }
--- frontend/agent-client/src/app/resources/pages/home/components/user-list/user-list.component.ts
+++ frontend/agent-client/src/app/resources/pages/home/components/user-list/user-list.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ModalController } from '@ionic/angular';
 
 import User from '@core/interfaces/user';
-import { ModalController } from '@ionic/angular';
+import { DepartmentService } from '@services/department/department.service';
+import { ToastService } from '@services/toast/toast.service';
+import Department from '@core/interfaces/department';
 
 @Component({
   selector: 'user-list',
@@ -12,28 +15,65 @@ import { ModalController } from '@ionic/angular';
 export class UserListComponent implements OnInit {
 
   users: User[]
-  selectedUser: User
+  selectedUser: any
   modalOpen: boolean = false
   titles = [ 'Nome', 'Matrícula', 'Secretaria', 'Ações' ]
 
+  account: User
+  deptList: any = null
+  newDept: String
+  oldDept: Department
+  editDepartment: boolean = false
+
   constructor( 
+    private _toast: ToastService,
     private _route: ActivatedRoute,
-    private _modal: ModalController ) { }
+    private _modal: ModalController,
+    private _deptService: DepartmentService ) { }
 
   ngOnInit() {
-    const current = JSON.parse( sessionStorage.getItem( 'user' ) )
+    this.account = JSON.parse( sessionStorage.getItem( 'user' ) )
+    this.setUserList()
+    if( this.account.department === 'Inova Macae' )
+      this.getDepartments()
+  }
+
+  setUserList() {
     this.users = this._route.snapshot.data.users.result
-    this.users = this.users.filter( el => el.username !== current.username )
+    this.users = this.users.filter( el => el.username !== this.account.username )
   }
 
   selectUser( user: User ) {
     this.selectedUser = user
     this.modalOpen = true
+    this.oldDept = user.department 
+    this.newDept= this.deptList[0].name
   }
 
   closeModal() {
     this._modal.dismiss()
     this.modalOpen = false
+  }
+
+  getDepartments() {
+    this.deptList = sessionStorage.getItem( 'deptList' )
+    if( this.deptList == null ) {
+      this._deptService.list()
+                .subscribe(
+                  res => this.deptList = res.result,
+                  err => this._toast.displayMessage( err ) )
+    }
+    else
+      this.deptList = JSON.parse( this.deptList )
+  }
+
+  hide( dept: Department ): boolean{
+    return dept.name === 'Inova Macae' ? true : false
+  }
+
+  save( user: User ) {
+    if( this.editDepartment )
+      user['department'].name = this.newDept
   }
 
 }
